bede01e3ecd2f638dceba152fb7bc78387894fe3
diaoko <diaoko89@gmail.com>
2019-08-20 17:41:46 +0430

    change some code to correct setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -17,13 +17,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// defining the Express app
-const app = express();
+
 
 // defining an array to work as the database (temporary solution)
 const ads = [
