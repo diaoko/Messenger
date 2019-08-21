596816a4362e6d5c2f844a4535270ee67c7f85d5
diaoko <diaoko89@gmail.com>
2019-08-21 17:37:04 +0430

    add new routes and related view

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/v1/getAllChats', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* get Send messages. */
+router.get('/v1/sendTextMessage', function(req, res, next) {
+  res.render('sendmessage', { title: 'Express' });
+});
+
+/* Post Send messages. */
+router.post('/v1/sendTextMessage', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
