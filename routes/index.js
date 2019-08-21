dde0adbfeea19c84b5bf74e756bc8540554fbc48
diaoko <diaoko89@gmail.com>
2019-08-21 18:10:34 +0430

    develop message model

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,20 @@ router.get('/v1/sendTextMessage', function(req, res, next) {
 
 /* Post Send messages. */
 router.post('/v1/sendTextMessage', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var messageSchema = require('../models/message').textmessage;
+  var chat= new messageSchema({
+        id : req.body.message_id,
+        chat_id : req.body.chat_id,
+        parse_mode : req.body.parse_mode,
+        reply_to : req.body.reply_to,
+        message : req.body.text,
+    });
+    chat.save(function (err) {
+        if(err)
+            console.log('error...');
+        else
+            console.log('ok......');
+    });
 });
 
 module.exports = router;
