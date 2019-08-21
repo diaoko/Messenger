ccd8c0ed65a997bfcfece5eecf645ead920267f6
diaoko <diaoko89@gmail.com>
2019-08-21 18:09:58 +0430

    add express-fileupload to project

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const helmet = require('helmet');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const uploadfile = require('express-fileupload');
+
 var app = express();
+
+app.use(uploadfile());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
