0e7121964a50d3c675d9280036c27dbe66816322
diaoko <diaoko89@gmail.com>
2019-08-25 16:09:03 +0430

    add message mapper to project

---
+++ utils/messagesMapper.js
@@ -0,0 +1,17 @@
+
+module.exports.messageViewModel =  messages => {
+    return {
+        messages_id: messages.id,
+        type : messages.type,
+        sender : {
+            id : '1dc4d7rf5vv5fvs',
+            type : 'user',
+            username: 'diaoko89',
+            first_name : 'diaoko',
+            last_name : 'mahmoodi'
+        },
+        'text_message' : {
+            text: messages.text_message.text
+        }
+    }
+};;
