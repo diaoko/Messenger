4ffea55686d9a33170ce25e4d41288be44bcd4ef
diaoko <diaoko89@gmail.com>
2019-08-27 00:40:36 +0430

    add condition to mapper

--- utils/messagesMapper.js
+++ utils/messagesMapper.js
@@ -1,18 +1,41 @@
 
 module.exports.messageViewModel =  messages => {
-    return {
-        messages_id: messages.id,
-        type : messages.type,
-        time : messages.createdAt,
-        sender : {
-            id : '1dc4d7rf5vv5fvs',
-            type : 'user',
-            username: 'diaoko89',
-            first_name : 'diaoko',
-            last_name : 'mahmoodi'
-        },
-        'text_message' : {
-            text: messages.text_message.text
+    if(messages.type.toString().trim()==='text_message'){
+
+        return {
+            messages_id: messages.id,
+            type : messages.type,
+            time : messages.createdAt,
+            sender : {
+                id : '1dc4d7rf5vv5fvs',
+                type : 'user',
+                username: 'diaoko89',
+                first_name : 'diaoko',
+                last_name : 'mahmoodi'
+            },
+            text_message : {
+                text: messages.text_message.text
+            }
+        }
+
+    }
+    else
+    {
+
+        return {
+            messages_id: messages.id,
+            type : messages.type,
+            time : messages.createdAt,
+            sender : {
+                id : '1dc4d7rf5vv5fvs',
+                type : 'user',
+                username: 'diaoko89',
+                first_name : 'diaoko',
+                last_name : 'mahmoodi'
+            },
+            voice : {
+                file_id: messages.voice.file_id
+            }
         }
     }
 };;
