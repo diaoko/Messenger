eb8f0eae9a07d0c647c355b1ef951a7949c7a0da
diaoko <diaoko89@gmail.com>
2019-09-03 18:11:07 +0430

    create file controller and move getFileById to it

---
+++ routes/fileController.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var myFile = require('../models/file');
+/**
+ *
+ * EndPoint = v1/getFileById/{id}
+ * Method = GET
+ *
+ * Description : this endpoint get id of file and return stream file for client to download
+ *  example : v1/getFileById/5d6b6d118446212a783d6393
+ *
+ */
+router.get('/v1/getFileById/:id',function (req,res,next) {
+    if(req.params.id==null)
+        res.json({haserror:true,code:404,msg: 'file not found'});
+    else
+    {
+        myFile.findOne({_id : req.params.id}).exec(function (err,file) {
+            if(file)
+                res.download(file.path);
+            else
+                res.json({haserror:true,code:404,msg: 'file not found'});
+        });
+    }
+
+});
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Message = require('../models/message');
 var Chat = require('../models/chat');
-var myFile = require('../models/file');
+
 let mapper = require('../utils/mapper');
 let messageMapper = require('../utils/messagesMapper');
 const shortid = require('shortid');
@@ -302,27 +302,4 @@ router.post('/v1/getMessages',function (req,res,next) {
         });
 });
 
-/**
- *
- * EndPoint = v1/getFileById/{id}
- * Method = GET
- *
- * Description : this endpoint get id of file and return stream file for client to download
- *  example : v1/getFileById/5d6b6d118446212a783d6393
- *
- */
-router.get('/v1/getFileById/:id',function (req,res,next) {
-    if(req.params.id==null)
-        res.json({haserror:true,code:404,msg: 'file not found'});
-    else
-    {
-        myFile.findOne({_id : req.params.id}).exec(function (err,file) {
-            if(file)
-                res.download(file.path);
-            else
-                res.json({haserror:true,code:404,msg: 'file not found'});
-        });
-    }
-
-});
 module.exports = router;
