265f4725afc0994f8b14823a3bdd13e1340e0059
diaoko <diaoko89@gmail.com>
2019-10-15 15:29:21 +0330

    add push model to project

---
+++ models/push.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let pushSchema = new Schema({
+    push : Object
+});
+
+module.exports = mongoose.model('Push',pushSchema,'Push');
